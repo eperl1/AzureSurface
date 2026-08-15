febbea9e07b4a1c2742a610bea0778ae8f5e50ac
Ethan Perlus-Rabinovitch <eperl1@ocdsb.ca>
2026-08-15 01:06:46 +0000

    Fix CI build breakages

--- windows/SurfaceModeReceiver/src/SurfaceModeReceiver/SurfaceModeServer.cs
+++ windows/SurfaceModeReceiver/src/SurfaceModeReceiver/SurfaceModeServer.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
